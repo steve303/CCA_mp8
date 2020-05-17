d6e89d9ec6149f9675d223a569930ce8b8d74088
yj shi <yjshi03@gmail.com>
2020-05-17 13:11:12 -0500

    Update MP3_PartE.py

--- python/MP3_PartE.py
+++ python/MP3_PartE.py
@@ -22,11 +22,11 @@ sqlContext = SQLContext(sc)
 # 5. Joining (10 points): The following program construct a new dataframe out of 'df' with a much smaller size.
 ####
 
-df2 = df.select("word", "count1").distinct().limit(1000);
+df2 = df.select("word", "count1").distinct().limit(100);
 df2.createOrReplaceTempView('gbooks2')
 
 # Now we are going to perform a JOIN operation on 'df2'. Do a self-join on 'df2' in lines with the same #'count1' values and see how many lines this JOIN could produce. Answer this question via DataFrame API and #Spark SQL API
 # Spark SQL API
 
-# output: 9658
+# output: 210
 
